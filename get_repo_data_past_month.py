f4e17cc1280e5b29ed5035823e189e4b1a2b2586
Andrea Picciau <andrea@picciau.net>
2022-02-08 23:31:10 +0000

    Inghitza a ndi fai sa versioni a meda lìnguas

--- get_repo_data_past_month.py
+++ get_repo_data_past_month.py
@@ -19,6 +19,48 @@
 from github import Github
 import datetime
 import sys
+import tempfile
+
+
+class LanguageWriter:
+    def __init__(self, language):
+        self.file = tempfile.NamedTemporaryFile(
+            mode='xw', prefix=language, delete=False)
+        self.language = language
+
+    def _add_line(self, line):
+        self.file.write(line)
+
+    def _add_newline(self):
+        self.file.write("\n")
+
+
+class SardinianWriter(LanguageWriter):
+    def __init__(self):
+        super().__init__("srd")
+
+    def print_post_header(self):
+        return
+
+    def print_repo_header(self, full_name, url):
+        self._add_line(f"## Traballu in sa faina [{full_name}]({url})")
+        return
+
+    def print_commit_header(self, sha, url, date):
+        self._add_line(f"[{sha}]({url}) fatu su {date}")
+        return
+
+    def print_commit_committer(self, name):
+        self._add_line(f"de **{committer_name}**")
+        return
+
+    def print_commit_author(self, login, url, name):
+        self._add_line(f"de **{committer_name}**")
+        return
+
+    def print_commit_stats(self):
+        return
+
 
 if __name__ == "__main__":
     github = Github(sys.argv[1])
@@ -39,12 +81,13 @@ if __name__ == "__main__":
             commit_url = commit.url
             log_string = [f"[{commit_sha}]({commit_url}) on"]
             commit_date = commit.commit.committer.date
-            if (today-commit_date).days>30:
+            if (today-commit_date).days > 30:
                 continue
 
             log_string += [f"{commit_date}"]
             if commit.author:
-                log_string += [f"by **[{commit.author.login}]({commit.author.url})** ({commit.author.name}):"]
+                log_string += [
+                    f"by **[{commit.author.login}]({commit.author.url})** ({commit.author.name}):"]
             elif commit.committer:
                 log_string += [f"by **{commit.committer.name}**:"]
             else:
